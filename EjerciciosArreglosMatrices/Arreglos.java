dbdcaed822486e800b266b9cc2d9d98ee2ff045f
Isa333-1 <lcalapsu@estudiante.uniajc.edu.co>
2026-09-10 03:05:23 +0000

    Corregi

--- EjerciciosArreglosMatrices/Arreglos.java
+++ EjerciciosArreglosMatrices/Arreglos.java
@@ -1,24 +1,40 @@
 package EjerciciosArreglosMatrices;
 
-public class Arreglos 
+public class Arreglos
 {
-    //Arreglos: 7, 8, 9, 10, 11, 12, 13 -> si le piden muchos números llene el arreglo con números aleatorios
-    //Ejercicio 7:
-    int [] A = {1,24,5,78,8,9,13,4,1,12};
-    int contadorPar;
-    int contadorImpar;
-    int [] par = new int[contadorPar];
-    int [] impar = new int[contadorImpar];
-
-    for (int i = 0; i < A.length; i++)
+    public static void main(String[] args) 
     {
-        if (A[i] % 2 == 0)
+        //Arreglos: 7, 8, 9, 10, 11, 12, 13 -> si le piden muchos números llene el arreglo con números aleatorios
+        //Ejercicio 7:
+        int [] A = {1,24,5,78,8,9,13,4,1,12};
+        int contadorPar = 0;
+        int contadorImpar = 0;
+        
+
+        for (int i = 0; i < A.length; i++)
         {
-            contadorPar++;
-        }else{
-            contadorImpar++;
+            if (A[i] % 2 == 0)
+            {
+                contadorPar++;
+            }else{
+                contadorImpar++;
+            }
+        }
+
+        int [] par = new int[contadorPar];
+        int [] impar = new int[contadorImpar];
+
+        for (int i = 0; i < A.length; i++)
+        {
+            if (A[i] % 2 == 0)
+            {
+                contadorPar++;
+            }else{
+                contadorImpar++;
+            }
         }
     }
+    
 
 
 
