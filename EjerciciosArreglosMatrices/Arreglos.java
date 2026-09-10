10a60e578bac27dfe897cb71d652cd07e71aa860
Isa333-1 <lcalapsu@estudiante.uniajc.edu.co>
2026-09-10 02:49:13 +0000

    Agregue contador de numeros pares e impares del arreglo A para definir el tamaño de los otros arreglos

--- EjerciciosArreglosMatrices/Arreglos.java
+++ EjerciciosArreglosMatrices/Arreglos.java
@@ -4,11 +4,24 @@ public class Arreglos
 {
     //Arreglos: 7, 8, 9, 10, 11, 12, 13 -> si le piden muchos números llene el arreglo con números aleatorios
     //Ejercicio 7:
-    int []A = {1,24,5,78,8,9,13,4,1,12};
-    int []par;
-    int []impar;
+    int [] A = {1,24,5,78,8,9,13,4,1,12};
+    int contadorPar;
+    int contadorImpar;
+    int [] par = new int[contadorPar];
+    int [] impar = new int[contadorImpar];
 
-    
+    for (int i = 0; i < A.length; i++)
+    {
+        if (A[i] % 2 == 0)
+        {
+            contadorPar++;
+        }else{
+            contadorImpar++;
+        }
+
+    }
+
+
 
 
     
