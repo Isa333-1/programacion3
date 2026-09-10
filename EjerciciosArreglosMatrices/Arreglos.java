072c642f387425dc993c171255b0571db16de41f
Isa333-1 <lcalapsu@estudiante.uniajc.edu.co>
2026-09-10 02:18:37 +0000

    Planteamiento

---
+++ EjerciciosArreglosMatrices/Arreglos.java
@@ -0,0 +1,15 @@
+package EjerciciosArreglosMatrices;
+
+public class Arreglos 
+{
+    //Arreglos: 7, 8, 9, 10, 11, 12, 13 -> si le piden muchos números llene el arreglo con números aleatorios
+    //Ejercicio 7:
+    int []A = {1,24,5,78,8,9,13,4,1,12};
+    int []par;
+    int []impar;
+
+    
+
+
+    
+}
