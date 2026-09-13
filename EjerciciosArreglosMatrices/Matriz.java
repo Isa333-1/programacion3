a9a59929585fb01c3833688ebd3b41828a40e499
Isa333-1 <lcalapsu@estudiante.uniajc.edu.co>
2026-09-13 23:57:28 +0000

    Declaracion de la matriz y ciclo for de multiplicacion del 1 al 10

--- EjerciciosArreglosMatrices/Matriz.java
+++ EjerciciosArreglosMatrices/Matriz.java
@@ -22,6 +22,42 @@ public class Matriz
         }
         System.out.print(cadena);
 
+
+        for (int i = 0; i < m.length; i++) 
+        {
+            int sumaFila = 0;
+            for (int j = 0; j < m[0].length; j++) 
+            {
+                sumaFila += m[i][j];
+            }
+            System.out.println("La suma de la fila " + i + " = " + sumaFila);
+        }
+
+        for (int j = 0; j < m[0].length; j++) 
+        {
+            int sumaCol = 0;
+            for (int i = 0; i < m.length; i++) 
+            {
+                sumaCol += m[i][j];
+            }
+            System.out.println("La suma de columna " + j + " = " + sumaCol);
+        }
+
+        //Ejercicio 18
+        int [][] t = new int[10][10]
+
+        for (int i = 0; i < 10; i++) 
+        {
+            for (int j = 0; j < 10; j++) 
+            {
+                t[i][j] = (i + 1) * (j + 1);    
+            }
+        }
+
+        
+
+        
+
     }
     
 }
