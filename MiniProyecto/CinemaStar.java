0e86607c20cb3fce7d24b75c9b41e19d4d5122ec
y4nka320 <gmejiam@estudiante.uniajc.edu.co>
2026-09-22 16:49:45 +0000

    creacion del la matriz que representa la sala de cine

--- MiniProyecto/CinemaStar.java
+++ MiniProyecto/CinemaStar.java
@@ -4,6 +4,16 @@ public class CinemaStar
 {
     public static void main(String[] args) 
     {
-        
+        String[][] sala1 = {{" ","1","2","3","4","5","6"},
+                            {"G"," ","_","X","X","_"," "},
+                            {"F"," ","_","_","_","_"," "},
+                            {"E","_","_","_","X","X","_"},
+                            {"D","_","_","_","_","_","_"}};
+        for(int i = 0; i < sala1.length; i++){
+            for(int j = 0; j < sala1[0].length; j++){
+                System.out.print(sala1[i][j] + " ");        
+            }
+            System.out.println("\n");
+        }
     }
 }
